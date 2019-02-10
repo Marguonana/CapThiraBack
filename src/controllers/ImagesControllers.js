4d5f1e1197413c53bc3f2b16b9359c84f70056cc
MarguoNana <n.marguerite@live.fr>
2019-02-10 18:53:25 +0100

    CapThira-430 : Implementer le POST dans imgController

--- src/controllers/ImagesControllers.js
+++ src/controllers/ImagesControllers.js
@@ -10,7 +10,20 @@ var Images = require('../models/ImagesModels');
  * Creation
  */
 exports.post = function(req,res){
+    var buffer = Buffer.from(req.b64string, 'base64')
     console.log("in: Post request\nreq: ", req);
+    mongoose.connect(mongoDB);
+    mongoose.Promise = global.Promise;
+    var db = mongoose.connection;
+    db.on('error',console.error.bind(console, 'erreur de connection à mongodb'));
+    db.once('open', function(){
+        console.log("Connexion à dbCapThira réussi")
+    });
+    var monImage = new Image({img: buffer,titre: req.title, idUser: req.id, datePublication: new Date(), taille: '1'})
+    monImage.save(function(err){
+    if(err){throw err;}
+    console.log('Image ajouté avec success !')
+  })
 }
 
 /**
