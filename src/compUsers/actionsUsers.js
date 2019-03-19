66318e7e2e0f6d2c74056c2c6f67907bd5514ea5
lahlou12 <lahloubena12@gmail.com>
2019-03-19 14:15:42 +0100

    Integration token et implémentation authentification

--- src/compUsers/actionsUsers.js
+++ src/compUsers/actionsUsers.js
@@ -1,9 +1,12 @@
 const colUsers  = require('./ModelsUsers');
 const processUsers = require('./processUsers');
+var jwt = require ('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 module.exports={
     
     showUserAction:(req,res)=>{ 
+        console.log("ici")
         processUsers.showUserProcess(req.params.id,res);
     },
     
@@ -16,27 +19,46 @@ module.exports={
     },
     
     addUserAction:(req,res)=>{
+        var pwd= bcrypt.hashSync(req.body.password, 8);
+        // creattion du token
+        var tokentoken = jwt.sign({nameUser:req.body.name, lastname:req.body.lastname, age:req.body.age, username:req.body.username}, 'SecretTopSecret');
+        console.log('req ', req.body);
+
         var myUser= new colUsers({
-            nameUser:req.body.nameUser,
-            lastname:req.body.lastname,
-            age:req.body.age,
-            username:req.body.username,
-            password: req.body.password,
-            token:''
-        })
+            nameUser: req.body.nameUser,
+            lastname: req.body.lastname,
+            age: req.body.age,
+            username: req.body.username,
+            password: pwd,
+            token: tokentoken
+        });
         processUsers.addUserProcess(myUser,res);
     },
 
     updateUserAction:(req,res)=>{
         var id = req.params.id
+        var pwd= bcrypt.hashSync(req.body.password, 8)
+        // creattion du token
+        var tokentoken = jwt.sign({nameUser:req.body.name, lastname:req.body.lastname, age:req.body.age, username:req.body.username}, 'SecretTopSecret');
+
         var myUser= new colUsers({
             nameUser:req.body.name,
             lastname:req.body.lastname,
             age:req.body.age,
             username:req.body.username,
-            password: req.body.password,
-            token:''
+            password: pwd,
+            token:tokentoken
         })
         processUsers.updateUserProcess(id,myUser,res)
+    },
+
+    authenticateUserAction: (req, res)=>{
+        // on a besoin de l'adresse mail et du mot de passe
+        processUsers.authenticateUserProcess(req.body.username,req.body.password,res);
+    },
+
+    verifTopkenAction: (req, res)=>{
+        var token = req.headers ['x-access-token'];
+        processUsers.verifTokenProcess(token,res);
     },
 }
--- src/compUsers/processUsers.js
+++ src/compUsers/processUsers.js
@@ -1,5 +1,7 @@
 const colUsers = require('./ModelsUsers');
 const ObjectId = require('mongodb').ObjectID
+var jwt = require ('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 module.exports={
     
@@ -27,6 +29,7 @@ module.exports={
     },
     
     addUserProcess:(myUser,res)=>{
+        console.log(myUser);
         myUser.save(function(err,user){
             if(err) return res.status(400).send('There was a problem adding the informations to the database.');
             res.status(200).send('User posted !/n'+ user)
@@ -37,11 +40,12 @@ module.exports={
         colUsers.findById({_id: ObjectId(id)}, function (err, user) {
             if (err) return res.status(400).send("There was a problem updating the user.");
             if (!user) return res.status(404).send("No User found.")
-            user.nameUser=myUser.name
-            user.lastname=myUser.lastname
-            user.age=myUser.age
-            user.username=myUser.username
-            user.password=myUser.password
+            user.nameUser= myUser.name
+            user.lastname= myUser.lastname
+            user.age= myUser.age
+            user.username= myUser.username
+            user.password= myUser.password
+            user.token= myUser.token
             user.save((err)=>{
                 if(err){
                     throw err
@@ -49,5 +53,32 @@ module.exports={
                 res.status(200).send('User Updated !')
             });      
         });
-    }
+    },
+
+    authenticateUserProcess: (userName,passWord, res)=>{
+        colUsers.findOne({username: userName}, (err, user)=>{
+            if(err) return res.status(500).send(userName);
+            if(!user) return res.status(404).send("No user found.");
+
+            var passewordIsValid= bcrypt.compareSync(passWord,user.password);
+            if (!passewordIsValid) return res.status(401).send({auth: false, token: null})
+            
+            res.status(200).send({auth:true, token: user.token})
+        })
+    },
+
+    verifTokenProcess: (token,res)=>{
+        if(!token) return res.status(401).send({auth: false, message:'undefined'});
+        
+        jwt.verify(token, 'SecretTopSecret', (err, decode)=>{
+            if(err) return res.status(500).send({auth: false, message:'undefined'});
+            
+            console.log('decodeeeee',decode);
+            colUsers.findOne({token: decode},(err, user)=> {
+                if (err) return res.status(400).send({auth: false,message: "undefined err"});
+                if (!user) return res.status(404).send({auth: false,message: "undefined, not user"});
+                res.status(200).send(JSON.stringify(user.token));
+            });
+        });
+    },
 }
