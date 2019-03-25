3849982059905afcd8e24137dbe1874eaac103c0
MarguoNana <n.marguerite@live.fr>
2019-03-25 20:34:50 +0100

    Security for aws

--- conf_AWS.js
+++ conf_AWS.js
@@ -2,8 +2,3 @@ const AWS = require('aws-sdk');
 
 //configuring the AWS environment 
 
-AWS.config.update({
-    accessKeyId:'AKIAIYATVZZOTGXNHTLA',
-    secretAccessKey:'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',
-    region: 'eu-west-1'
-});
