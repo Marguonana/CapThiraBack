0953d71476e039be759501aa7ed3302eb68ca15d
unknown <mohandamezianezaidi@gmail.com>
2019-05-27 14:57:58 +0200

    CapThira-1131:  recherche intelligente (Majuscule/minuscule)

--- src/compUsers/actionsUsers.js
+++ src/compUsers/actionsUsers.js
@@ -14,13 +14,14 @@ module.exports={
             nameUser: req.body.nameUser,
             lastname: req.body.lastname,
             age: req.body.age,
-            pseudo:req.body.pseudo,
+            pseudo:req.body.pseudo.toLowerCase(),
             username: req.body.username,
             password: pwd,
-            listAbbo: ["5ca2685087cf0102f401068a","5ca2685087cf0102f401068b"],
+            subscribe: [],
+            subscriber: [],
             token: tkn
         });
-
+        console.log(myUser.pseudo)
         processUsers.addUserProcess(myUser)
         .then((result)=>{
             res.status(200).json(result)
@@ -45,6 +46,7 @@ module.exports={
     
     showAllUsersAction:(req,res)=>{ 
         pseudo=req.params.pseudo
+        pseudo=pseudo.toLowerCase()
         processUsers.showAllUsersProcess(pseudo)
         .then((result)=>{
             res.status(200).json(result)
